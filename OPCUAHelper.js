7a2e59e6d9069c7c112076c9da62a6274b82c5b1
Weston Baxter <wbaxter@clearblade.com>
2022-09-20 10:16:13 -0500

    find() polyfill for BH platform

--- OPCUAHelper.js
+++ OPCUAHelper.js
@@ -2,7 +2,29 @@
  * Type: Library
  * Description: A library that contains a function which, when called, returns an object with a public API.
  */
+
+//TODO accept an array instead of just a single path and display name
 function OPCUAHelper(browseResponse) {
+  //find() polyfill
+  Array.prototype.find =
+    Array.prototype.find ||
+    function (callback) {
+      if (this === null) {
+        throw new TypeError("Array.prototype.find called on null or undefined");
+      } else if (typeof callback !== "function") {
+        throw new TypeError("callback must be a function");
+      }
+      var list = Object(this);
+      // Makes sures is always has an positive integer as length.
+      var length = list.length >>> 0;
+      var thisArg = arguments[1];
+      for (var i = 0; i < length; i++) {
+        var element = list[i];
+        if (callback.call(thisArg, element, i, list)) {
+          return element;
+        }
+      }
+    };
   browseResponse.nodes.sort(compare);
   var _nodeTree = null;
   _populateNodeTree(browseResponse);
